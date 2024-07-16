a8f822d4b3f0648d89fc9d5f8bc0ebdbfba984df
daumnulunggi <inulunggi@daum.net>
2024-07-16 13:16:18 +0900

    열심히합시다.

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -32,6 +32,8 @@ public class CICDCont {
     } else if (sw == 2) {
       msg = "/cide workflow 테스트";
       log.info(msg);
+    } else {
+      msg = "해당하는 sw 변수의 값이 없습니다.";
     }
     
     return "<h3>" + msg + "</h3>";
